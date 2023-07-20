414af25ef875585bc51aaf11580e05e3a6454e95
orianamachado66 <orianamachado66@gmail.com>
2023-07-19 22:10:58 -0700

    Added the JavaScript for the select element

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,13 +1,17 @@
+//Updating the real time and date for the three cities displayed on the page
+
 function updateLosAngelesTime(event) {
   //Los Angeles
   let losAngelesElement = document.querySelector("#los-angeles");
-  let losAngelesDateElement = losAngelesElement.querySelector(".date"); // this selects the class of date inside of the los-angeles id
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time"); // this selects the class of time inside of the los-angeles id
-  let losAngelesTime = moment().tz("America/Los_Angeles");
-  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do, YYYY");
-  losAngelesTimeElement.innerHTML = losAngelesTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  ); // This keeps the AM on the side and smaller
+  if (losAngelesElement.length) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date"); // this selects the class of date inside of the los-angeles id
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time"); // this selects the class of time inside of the los-angeles id
+    let losAngelesTime = moment().tz("America/Los_Angeles");
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do, YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    ); // This keeps the AM on the side and smaller
+  }
 }
 updateLosAngelesTime(); // by calling the function here, the data will already be there when the page loads
 setInterval(updateLosAngelesTime, 1000);
@@ -41,3 +45,27 @@ function updateLisbonTime(event) {
 
 updateLisbonTime();
 setInterval(updateLisbonTime, 1000);
+
+//Drop down select menu then changing the inner html to display each time and date
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value; // this goes to the value inside of each option in the select element, so we get the timezone for each city
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1]; // this line of code takes the time zone name, replaces underscores with spaces, splits it into separate parts using slashes, and then retrieves the second part, which is the name of the city. This city name is then stored in the cityName variable for further use.
+  let cityTime = moment().tz(cityTimeZone); // getting the timezone for the selected city and storing it in a variable called cityTime
+  let htmlCityDisplay = document.querySelector("#cities");
+  //whenever the user selects a city the inner html below will change and display the name, time and date of that city in current time
+  htmlCityDisplay.innerHTML = `
+  <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss"
+          )} <small> ${cityTime.format("A")}</small></div>
+        </div>
+        `;
+}
+
+let citiesSelectElement = document.querySelector("#drop-down-cities"); // we select the element we want to target
+citiesSelectElement.addEventListener("change", updateCity); // everytime the user changes the city, the function updateCity runs
